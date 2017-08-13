3282d332f4ed2bd9538450dea78a64a9d4ca235b
Renan Thiago <renannojosa@gmail.com>
2017-08-12 21:58:03 -0300

    Implementando login pelo Google+

--- app/src/main/java/com/example/dell/fichacadastral/Deliverer_Activity.java
+++ app/src/main/java/com/example/dell/fichacadastral/Deliverer_Activity.java
@@ -1,18 +1,31 @@
 package com.example.dell.fichacadastral;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.model.people.Person;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
 
 /**
  * Created by Dell on 05/08/2017.
@@ -22,13 +35,14 @@ import android.view.MenuItem;
 
 public class Deliverer_Activity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener, Delivery_Fragment.onModifyFragment,
-            Profile_Fragment.onModifyFragment{
+        Profile_Fragment.onModifyFragment {
     private static final String EXTRA_CUSTOMER = "customer"; // Primary Key
     private NavigationView navigationView;
     private DrawerLayout drawerLayout;
     private ActionBarDrawerToggle actionBarDrawerToggle; // used to open and quit the lateral menu
     private int selectedOption;
     private Customer costumer;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -146,4 +160,53 @@ public class Deliverer_Activity extends AppCompatActivity implements
     public void saveAllModifications(Customer customer) {
         this.costumer = customer; //Receives all changes made in the fragment
     }
+
+
+    private void upDateMenu() {
+        final ImageView imgCapa = (ImageView) findViewById(R.id.imgCapa);
+        final ImageView imgFoto = (ImageView) findViewById(R.id.imgFotoPerfil);
+        final TextView txtNome = (TextView) findViewById(R.id.txtNome);
+        if (googleApiClient.isConnected()) {
+            Person person = Plus.PeopleApi.getCurrentPerson(googleApiClient);
+            if (person != null) {
+                txtNome.setText(person.getDisplayName());
+                if (person.hasImage()) {
+                    Target target = new Target() {
+                        @Override
+                        public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom loadedFrom) {
+                            RoundedBitmapDrawable fotoRedonda = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+                            fotoRedonda.setCornerRadius(Math.max(bitmap.getWidth(), bitmap.getHeight()) / 2.0f);
+                            imgFoto.setImageDrawable(fotoRedonda);
+                        }
+
+                        @Override
+                        public void onBitmapFailed(Drawable drawable) {
+                        }
+
+                        @Override
+                        public void onPrepareLoad(Drawable drawable) {
+                        }
+                    };
+                    Picasso.with(this)
+                            .load(person.getImage().getUrl())
+                            .into(target);
+                }
+                if (person.hasCover()) {
+                    Picasso.with(this)
+                            .load(person.getCover().getCoverPhoto().getUrl())
+                            .into(imgCapa);
+                }
+            }
+        } else {
+            imgCapa.setImageBitmap(null);
+            txtNome.setText(R.string.app_name);
+            imgFoto.setImageResource(R.mipmap.ic_launcher);
+        }
+        navigationView.getMenu()
+                .findItem(R.id.action_login_logout)
+                .setTitle(googleApiClient.isConnected() ?
+                        "Logout" : "Login");
+    }
+
+
 }
--- app/src/main/java/com/example/dell/fichacadastral/MainActivity.java
+++ app/src/main/java/com/example/dell/fichacadastral/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.dell.fichacadastral;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+    private SignInButton signInButton;
     private Button button;
+    private int selectedOption;
+    public static final int REQUEST_SIGN_IN = 0;
+    private GoogleApiClient googleApiClient;
+    private boolean IntentInProgress;
+    private boolean SignInClicked;
+    private ConnectionResult mConnectionResult;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (googleApiClient.isConnected()) {
+            googleApiClient.disconnect();
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,7 +70,56 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, REQUEST_SIGN_IN);
+            }
+        });
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 
 
+    @Override
+    public void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        super.onActivityResult(requestCode, responseCode, intent);
+        if (requestCode == REQUEST_SIGN_IN) {
+            if (responseCode != Activity.RESULT_OK) {
+                Toast.makeText(this, R.string.sucsses_msg_01, Toast.LENGTH_SHORT).show();
+                intent = new Intent(this, Deliverer_Activity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, R.string.error_msg_01, Toast.LENGTH_SHORT).show();
+            }
+
+            if (!googleApiClient.isConnecting()) {
+                googleApiClient.connect();
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
