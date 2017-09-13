04551490155dba3ad0a9a9f651412c866303219f
Francisco <francisco_nascimento@outlook.com.br>
2017-09-13 08:39:19 -0300

    Foi implementado a requisição do token do entregador e um alert dialog com a exibição deste.

--- app/src/main/java/com/example/dell/fichacadastral/Exibir_Solicitacoes.java
+++ app/src/main/java/com/example/dell/fichacadastral/Exibir_Solicitacoes.java
@@ -1,33 +1,28 @@
 package com.example.dell.fichacadastral;
 
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -39,33 +34,30 @@ import javax.net.ssl.HttpsURLConnection;
 
 public class Exibir_Solicitacoes  extends AppCompatActivity{
 
+    private List<Solicitacoes_Entregador> obj = null;
+    private AlertDialog alerta;//atributo da classe.
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.exibir_solicitacoes);
         new SendPostRequest().execute();
 
-
-
     }
 
-   /* private List<Solicitacoes_Entregador> listarSolicitacoes(Solicitacoes_Entregador solicitacoesEntregador) {
-        return new ArrayList<>(Arrays.asList(
-                new Solicitacoes_Entregador(solicitacoesEntregador.getId(),"Teste","Teste","Teste",0,"Teste","Teste","Teste","Teste","Teste",0)));
-
-    }
-*/
     public class SendPostRequest extends AsyncTask<String, Void, String> {
+
+        private String idEntregador = "1";
 
         protected void onPreExecute(){}
 
         protected String doInBackground(String... arg0) {
 
             try {
-
                 URL url = new URL("https://smart-delivery-labes.herokuapp.com/api/entregador/listarSolicitacoes/"); // here is your URL path
 
                 JSONObject postDataParams = new JSONObject();
-                postDataParams.put("idEntregador", "1");
+                //TODO fazer a classe obter automaticamente o id do entregador
+                postDataParams.put("idEntregador", idEntregador);
                 Log.e("params",postDataParams.toString());
 
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -112,41 +104,121 @@ public class Exibir_Solicitacoes  extends AppCompatActivity{
             catch(Exception e){
                 return new String("Exception: " + e.getMessage());
             }
-
         }
 
         @Override
-        protected void onPostExecute(String result) {
-            //Todo tratar aqui a string para ser exibida na listview
-            /*ListView listaDeSolicitacoes = (ListView) findViewById(R.id.lista_solicitacoes);
-            Solicitacoes_Entregador solicitacoesEntregador = new Solicitacoes_Entregador();
-            solicitacoesEntregador.setId("Junior");
-
-            List<Solicitacoes_Entregador> solicitacoes_entregadores = listarSolicitacoes(solicitacoesEntregador);
-            ArrayAdapter<Solicitacoes_Entregador> adapter = new ArrayAdapter<>(Exibir_Solicitacoes.this, android.R.layout.simple_list_item_1, solicitacoes_entregadores);
-            listaDeSolicitacoes.setAdapter(adapter);
-
-            Toast.makeText(getApplicationContext(), result,
-                    Toast.LENGTH_LONG).show();
-                    */
-
-
+        protected void onPostExecute(final String result) {
             ListView listView = (ListView)findViewById(R.id.lista_solicitacoes);
 
-            List<Solicitacoes_Entregador> obj = null;
             try {
-                obj = gerarSolicitacoes(result);
+                obj = gerarSolicitacoes(result); //obj recebe um list com as solicitações
             } catch (JSONException e) {
                 e.printStackTrace();
             }
             ArrayAdapter<Solicitacoes_Entregador> solicitacoesAdapter = new ArrayAdapter<Solicitacoes_Entregador>(Exibir_Solicitacoes.this, android.R.layout.simple_list_item_1, obj);
             listView.setAdapter(solicitacoesAdapter);
+
+            //Torna o list view clicável
+
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+                    new SendPostRequestToken().execute();
+                    //Código para o clique
+                    /*Toast.makeText(getApplicationContext(), obj.get(0).getId(),
+                            Toast.LENGTH_LONG).show();*/
+                }
+            });
+        }
+    }
+
+    //Token
+    public class SendPostRequestToken extends AsyncTask<String, Void, String> {
+
+
+        protected void onPreExecute(){}
+
+        protected String doInBackground(String... arg0) {
+
+            try {
+                URL url = new URL("https://smart-delivery-labes.herokuapp.com/api/solicitante/getTokensConfirmacao/"); // here is your URL path
+
+                JSONObject postDataParams = new JSONObject();
+
+                postDataParams.put("idSolicitacao", obj.get(0).getId());
+                Log.e("params",postDataParams.toString());
+
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(15000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                OutputStream os = conn.getOutputStream();
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(os, "UTF-8"));
+                writer.write(getPostDataString(postDataParams));
+
+                writer.flush();
+                writer.close();
+                os.close();
+
+                int responseCode=conn.getResponseCode();
+
+                if (responseCode == HttpsURLConnection.HTTP_OK) {
+
+                    BufferedReader in=new BufferedReader(new
+                            InputStreamReader(
+                            conn.getInputStream()));
+
+                    StringBuffer sb = new StringBuffer("");
+                    String line="";
+
+                    while((line = in.readLine()) != null) {
+
+                        sb.append(line);
+                        break;
+                    }
+
+                    in.close();
+                    return sb.toString();
+
+                }
+                else {
+                    return new String("false : "+responseCode);
+                }
+            }
+            catch(Exception e){
+                return new String("Exception: " + e.getMessage());
+            }
+        }
+
+        @Override
+        protected void onPostExecute(final String result) {
+            try {
+                JSONObject jsonObjeto = new JSONObject(result);
+                String tokenEntregador = jsonObjeto.getString("tokenEntrega");
+
+                /*Toast.makeText(getApplicationContext(), tokenEntregador,
+                        Toast.LENGTH_LONG).show();*/
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(Exibir_Solicitacoes.this);//Cria o gerador do AlertDialog
+                builder.setTitle("Token");//define o titulo
+                builder.setMessage(tokenEntregador);//define a mensagem
+
+                alerta = builder.create();//cria o AlertDialog
+                alerta.show();//Exibe
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     private List<Solicitacoes_Entregador> gerarSolicitacoes(String result) throws JSONException {
 
-            //Todo implementar aqui o array com os dados
             JSONObject objetoJSON = new JSONObject(result);
             JSONArray jsonArray = objetoJSON.getJSONArray("solicitacoes");
 
@@ -167,23 +239,16 @@ public class Exibir_Solicitacoes  extends AppCompatActivity{
                 solEnt.setValorTaxaServico(e.getDouble("valorTaxaServico"));
 
             }
-
-
-
             //solEnt.setId(objetoJSON.getString("solicitacoes"));//esta linha sera removida
-
             /*Toast.makeText(getApplicationContext(), solEnt.getId(),
                     Toast.LENGTH_LONG).show();*/
-
 
         List<Solicitacoes_Entregador> solicitacoes = new ArrayList<Solicitacoes_Entregador>();
         for(int i = 0; i<1; i++) {
             solicitacoes.add(solEnt);
         }
-
         return solicitacoes;
     }
-
 
     public String getPostDataString(JSONObject params) throws Exception {
 
@@ -209,5 +274,4 @@ public class Exibir_Solicitacoes  extends AppCompatActivity{
         }
         return result.toString();
     }
-
 }
